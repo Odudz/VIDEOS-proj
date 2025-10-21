79b89c3b659d7ab5cb9e6559a5fe96d6a138c56a
Dudz <142178060+Odudz@users.noreply.github.com>
2025-10-21 09:40:57 -0300

    .

--- backend/server.js
+++ backend/server.js
@@ -36,39 +36,70 @@ app.post('/auth/login', async(req, res) => {
     }
 
     const user = await database.findByEmail(email);
-    if(user){
-      return res.status(400).json({ msg: 'Usuário não encontrado!'})
+    if(!user){
+        return res.status(400).json({ msg: 'Usuário não encontrado'})
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if(!isPasswordValid){
-      return res.status(401).json({ msg: 'Senha inválida!'})
+        return res.status(401).json({ msg: 'Senha inválida!'});
     }
 
-    const token = jwt.sign(
-      { id: user.id, email: user.email},
-      process.env.JWT_SECRET || 'MinhaSenhaSecreta',
-      {expiresId: 'id'}
+    const token = jwt.sing(
+        {id: user.id, email: user.email},
+        process.env.JWT_SECRET || 'minhaChaveSuperSecreta',
+        {expiresId: 'id'}
     );
 
     res.json({
-      msg: 'Login realizado com sucesso!',
-      token,
-      user:{id: user.id, name: user.name, email:user.email }
+        msg:'Login realizado!',
+        token,
+        user: {id: user.id, name: user.name, email: user.email}
     });
-})
+});
 
-app.get('/protected', (req,res) =>{
-  const authHeader = req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).json({ msg: 'Token não fornecido!'});
-  }
+app.get('/protected', (req, res) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).json({ msg: 'Token não fornecido!'});
+    }
+    
+        const token = authHeader.split(' ')[1];
+        try{
+            const decoded = jwt.verify(token, process.env.JWT_SECRET || 'MinhaChaveSuperSecreta');
+            res.json({ msg: "Acesso autorizado!, decoded"});
+        }catch(err){
+            res.status(401).json({ msg: 'Token Inválido!'});
+        }
+});
 
-  const token = authHeader.split(' ')[1];
-  try{
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'MinhaChaveSecreta');
-    res.json({ msg: 'Acesso autorizado!', decoded});
-  }catch(err){
-    res.status(401).json({ msg: 'Token inválido!'});
-  }
+app.get('/users', async(req, res) => {
+    const users = await database.lsit();
+    res.json(users);
+});
+
+app.post('/users', async(req, res) => {
+    const { name, email, password} = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({ msg: 'Preencha todos os campos! '});
+    }
+    await database.create({ name, email, password});
+    res.status(201).send();
+});
+
+app.put('/users/:id', async(req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+    await database.update(id, user);
+    res.status(204).send();
+});
+
+app.delete('/users/:id', async(req, res) => {
+    const id = req.params.id;
+    await database.delete(id);
+    res.status(204).send();
+});
+
+app.listen(3001, () => {
+    console.log('Servidor rodando na porta 3001');
 });
