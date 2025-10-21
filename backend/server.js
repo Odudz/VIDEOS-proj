ad7e75e34a3f60e06a718c9f95cab58f4ecc1090
Dudz <142178060+Odudz@users.noreply.github.com>
2025-10-21 07:54:44 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { DatabasePostgres } from ' ./databasePostgres';
+import { databasePostgres } from './databasePostgres';
 import './createTable.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -12,26 +12,63 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const database = new DatabasePostgres();
+const database = new databasePostgres();
 
-app.post('/auth/register', async(req,res) =>{
-   const { name,email,password } = req.body;
-  if(!name || !email || !passowrd){
-    return res.status(400).json({ msg: 'Preeencha todos os campos!' });
-  }
-
-  const exisistingUser = await database.findByEmail(email);
-  if(exisistingUser){
-    return.res.status(400).json({ msg: 'Email já encontrado! });
-  }
-
-  await database.create({ name,email,password});
-  res.status(201).json({ msg: 'Usuário criado!'});
-
-   app.post('/auth/login', async(req, res) => {
-      const { email, password } = req.body;
-      if(!email || !password){
-      return res.status(400).json({ msg: 'Preencha email e senha!'});
+app.post('/auth/register', async(req, res) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({ msg: 'Preencha todos os campos! '});
     }
+
+    const existingUser = await database.findByEmail(email);
+    if(existingUser){
+        return res.status(400).json({ msg: 'Email já cadastrado!'});
+    }
+
+    await database.create({ name, email, password});
+    res.status(201).json({ msg: 'Usuário criado!'});
 })
 
+app.post('/auth/login', async(req, res) => {
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({ msg: 'Preencha email e senha!'});
+    }
+
+    const user = await database.findByEmail(email);
+    if(user){
+      return res.status(400).json({ msg: 'Usuário não encontrado!'})
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+      return res.status(401).json({ msg: 'Senha inválida!'})
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email},
+      process.env.JWT_SECRET || 'MinhaSenhaSecreta',
+      {expiresId: 'id'}
+    );
+
+    res.json({
+      msg: 'Login realizado com sucesso!',
+      token,
+      user:{id: user.id, name: user.name, email:user.email }
+    });
+})
+
+app.get('/protected', (req,res) =>{
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).json({ msg: 'Token não fornecido!'});
+  }
+
+  const token = authHeader.split(' ')[1];
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'MinhaChaveSecreta');
+    res.json({ msg: 'Acesso autorizado!', decoded});
+  }catch(err){
+    res.status(401).json({ msg: 'Token inválido!'});
+  }
+});
