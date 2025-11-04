86ffc6f3481fa4424a77b3e6702cf7a816e79694
Dudz <142178060+Odudz@users.noreply.github.com>
2025-11-04 09:40:12 -0300

    Add files via upload

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,2 +1,12 @@
 import './Footer.css';
 import React from 'react';
+
+const Footer = () =>(
+    <footer className='footer'>
+        <span>
+            desenvolvido por Eduardo Pimenta
+        </span>
+    </footer>
+);
+
+export default Footer;
