1cdb01194d6033c22779796ad9739893ccf9f2d5
Dudz <142178060+Odudz@users.noreply.github.com>
2025-11-07 08:53:49 -0300

    Add files via upload

---
+++ frontend/src/components/template/Home/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main  from "../template/Main";
+
+const Home = () => (
+    <Main icon="Home" title="Início" subtitl="Primeiro Projeto de capitulo React.">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">sisema para exemplificar a construção de um projeto desenvolvido em react!</p>
+    </Main>
+)
+
+export default Home;
